5dbfe0b5ca8be2a5cb1429de69c80f350ac2b9d5
Shashank <shashanksaxena226@gmail.com>
2022-04-10 16:30:47 +0530

    Initial commit

---
+++ LinkedList/menu.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <process.h>
+typedef struct lkList
+{
+    int info;
+    struct lkList *next;
+} node;
+void main()
+{
+    while (1)
+    {
+        int n = 0, data = 0;
+        node *temp = NULL;
+        node *head = NULL;
+        printf("\nEnter your choice\n");
+        printf("1 for insert an element\n");
+        printf("13 for exit\n");
+        scanf("%d", &n);
+        if (n == 13)
+            exit(1);
+        switch (n)
+        {
+        case 1:
+            printf("Enter the data you want to be insert\n");
+            scanf("%d", &data);
+            temp = (node *)malloc(sizeof(node));
+            if (temp == NULL)
+            {
+                printf("Can't assign value\n");
+                break;
+            }
+            temp->info = data;
+            temp->next = head;
+            head = temp;
+            printf("Node insert successfully");
+            break;
+
+        default:
+            break;
+        }
+    }
+}
