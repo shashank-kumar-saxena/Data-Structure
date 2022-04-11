4d5ba293853c62a4ceafb07a5e323b942ced1c8f
Shashank Kumar <69252042+shashank-kumar-saxena@users.noreply.github.com>
2022-04-11 14:39:39 +0530

    Update menu.c

--- LinkedList/menu.c
+++ LinkedList/menu.c
@@ -10,12 +10,16 @@ void main()
 		int n = 0, data = 0;
 		node *temp = NULL;
 		node *head = NULL;
+		node *end=NULL;
+		int count=0;
 		clrscr();
 	while (1)
 	{
 		printf("\nEnter your choice\n");
 		printf("1 for insert an element\n");
 		printf("2 for traversing\n");
+		printf("3 for insert at end\n");
+		printf("4 for search the element\n");
 		printf("13 for exit\n");
 		scanf("%d", &n);
 		if (n == 13)
@@ -41,13 +45,55 @@ void main()
 			 if(temp==NULL)
 			 {
 			  printf("No data found\n");
+			  break;
 			 }
 			 while(temp!=NULL)
 			 {
-			 printf("Value of node is %d\n",temp->info);
+			 printf("%d ",temp->info);
 			 temp=temp->next;
 			 }
 			break;
+			case 3:
+			   printf("Enter the element you want to insert at end\n");
+			   scanf("%d",&data);
+			   end=(node *)(malloc(sizeof(node)));
+			   end->info=data;
+			   temp=head;
+			   while(temp->next!=NULL)
+			   {
+			   temp=temp->next;
+			   }
+			   if(temp->next==NULL)
+			   {
+			   temp->next=end;
+			   end->next=NULL;
+			   printf("Node insert successfully\n");
+			   }
+			   break;
+			   case 4:
+			   temp=head;
+			   count=0;
+			   printf("Enter the data you want to search\n");
+			   scanf("%d",&data);
+			   while(temp!=NULL)
+			   {
+				if(temp->info==data)
+				{
+				 count=1;
+				 break;
+
+				}
+				temp=temp->next;
+			   }
+			   if(count==1)
+				{
+				 printf("Element found\n");
+				}
+				else
+				{
+				 printf("Element not found\n");
+				}
+				break;
 		default:
 			printf("Enter the valid choice");
 			break;
