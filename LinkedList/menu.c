e972d8e64d9da49a19d6df4972824928bd2b124c
Shashank Kumar <69252042+shashank-kumar-saxena@users.noreply.github.com>
2022-04-11 13:51:49 +0530

    Update menu.c

--- LinkedList/menu.c
+++ LinkedList/menu.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <process.h>
 typedef struct lkList
 {
     int info;
@@ -8,36 +7,50 @@ typedef struct lkList
 } node;
 void main()
 {
-    while (1)
-    {
-        int n = 0, data = 0;
-        node *temp = NULL;
-        node *head = NULL;
-        printf("\nEnter your choice\n");
-        printf("1 for insert an element\n");
-        printf("13 for exit\n");
-        scanf("%d", &n);
-        if (n == 13)
-            exit(1);
-        switch (n)
-        {
-        case 1:
-            printf("Enter the data you want to be insert\n");
-            scanf("%d", &data);
-            temp = (node *)malloc(sizeof(node));
-            if (temp == NULL)
-            {
-                printf("Can't assign value\n");
-                break;
-            }
-            temp->info = data;
-            temp->next = head;
-            head = temp;
-            printf("Node insert successfully");
-            break;
-
-        default:
-            break;
-        }
-    }
+		int n = 0, data = 0;
+		node *temp = NULL;
+		node *head = NULL;
+		clrscr();
+	while (1)
+	{
+		printf("\nEnter your choice\n");
+		printf("1 for insert an element\n");
+		printf("2 for traversing\n");
+		printf("13 for exit\n");
+		scanf("%d", &n);
+		if (n == 13)
+			exit(1);
+		switch (n)
+		{
+		case 1:
+			printf("Enter the data you want to be insert\n");
+			scanf("%d", &data);
+			temp = (node *)malloc(sizeof(node));
+			if (temp == NULL)
+			{
+				printf("Can't assign value\n");
+				break;
+			}
+			temp->info = data;
+			temp->next = head;
+			head = temp;
+			printf("Node insert successfully");
+			break;
+		 case 2:
+			 temp=head;
+			 if(temp==NULL)
+			 {
+			  printf("No data found\n");
+			 }
+			 while(temp!=NULL)
+			 {
+			 printf("Value of node is %d\n",temp->info);
+			 temp=temp->next;
+			 }
+			break;
+		default:
+			printf("Enter the valid choice");
+			break;
+		}
+	}
 }
