17a38ce70682a1045249041a3cf7a88cdab85812
Shashank <shashanksaxena226@gmail.com>
2022-04-15 17:02:13 +0530

    comment section added

--- Linked List using Function/main.c
+++ Linked List using Function/main.c
@@ -1,12 +1,15 @@
 // by shashank
 #include <stdio.h>
 #include <stdlib.h>
+// Single linked list define
 typedef struct lklist
 {
     int info;
     struct lklist *next;
 } node;
+// globle variable decleration
 node *temp = NULL;
+// function prototyping
 void insertAtBeg(node **head, int data);
 void insertAtEnd(node *head, int data);
 void traversing(node *head);
@@ -31,10 +34,10 @@ int main()
         case 1:
             printf("Enter the element you want to insert\n");
             scanf("%d", &data);
-            insertAtBeg(&head, data);
+            insertAtBeg(&head, data); // function calling
             break;
         case 5:
-            traversing(head);
+            traversing(head); // calling
             break;
         default:
             printf("Enter the valid choice\n");
@@ -43,6 +46,7 @@ int main()
     }
     return (1);
 }
+// function decleration of insert at begning
 void insertAtBeg(node **head, int data)
 {
     temp = (node *)(malloc(sizeof(node)));
@@ -51,6 +55,7 @@ void insertAtBeg(node **head, int data)
     *head = temp;
     printf("node insert successfully\n");
 }
+// function decleration of traversing
 void traversing(node *head)
 {
     while (head != NULL)
