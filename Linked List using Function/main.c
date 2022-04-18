1a94b1dcd282f66b05a0c461cba1f54d2abc53f2
Shashank Kumar <69252042+shashank-kumar-saxena@users.noreply.github.com>
2022-04-18 14:23:54 +0530

    Update main.c

--- Linked List using Function/main.c
+++ Linked List using Function/main.c
@@ -4,11 +4,12 @@
 // Single linked list
 typedef struct lklist
 {
-    int info;
-    struct lklist *next;
+	int info;
+	struct lklist *next;
 } node;
 // globle variable decleration
 node *temp = NULL;
+node *ptr=NULL;
 // function prototyping
 void insertAtBeg(node **head, int data);              // case 1
 void insertAtEnd(node **head, int data);              // case 2
@@ -20,118 +21,118 @@ void deleteSpecificPosition(node **head, int data);   // case 7 //here data vari
 int main()
 {
 
-    int n = 0, data = 0;
-    node *head = NULL;
-    while (1)
-    {
-        printf("\nEnter your choice\n");
-        printf("1 for insert at begning\n");
-        printf("2 for insert at end\n");
-        printf("3 for insert after specific element\n");
-        printf("4 for insert at specific element\n");
-        printf("5 for traversing\n");
-        printf("6 for delete a element\n");
-        printf("7 for delete a element at specific element\n");
-        printf("13 for exit\n");
-        scanf("%d", &n);
-        if (n == 13)
-        {
-            exit(1);
-        }
-        switch (n)
-        {
-        case 1:
-            printf("Enter the element you want to insert\n");
-            scanf("%d", &data);
-            insertAtBeg(&head, data); // function calling
-            break;
-        case 2:
-            printf("Enter the element you want to insert\n");
-            scanf("%d", &data);
-            insertAtEnd(&head, data); // function calling
-            break;
-        case 3:
-            printf("Enter element after which you want to insert the element\n");
-            scanf("%d", &data);
-            insertAfterSpecific(&head, data);
-            break;
-        case 4:
-            printf("Enter the position where you want to insert the element");
-            scanf("%d", &data);
-            insertAtSpecificPosition(&head, data);
-            break;
-        case 5:
-            traversing(head); // calling
-            break;
-        case 6:
-            printf("Enter the element you want to delete\n");
-            scanf("%d", &data);
-            deleteSpecific(&head, data);
-            break;
-        case 7:
-            printf("Enter the position of element which you want to delete\n");
-            scanf("%d", &data);
-            deleteSpecificPosition(&head, data);
-            break;
+	int n = 0, data = 0;
+	node *head = NULL;
+	while (1)
+	{
+		printf("\nEnter your choice\n");
+		printf("1 for insert at begning\n");
+		printf("2 for insert at end\n");
+		printf("3 for insert after specific element\n");
+		printf("4 for insert at specific element\n");
+		printf("5 for traversing\n");
+		printf("6 for delete a element\n");
+		printf("7 for delete a element at specific element\n");
+		printf("13 for exit\n");
+		scanf("%d", &n);
+		if (n == 13)
+		{
+			exit(1);
+		}
+		switch (n)
+		{
+		case 1:
+			printf("Enter the element you want to insert\n");
+			scanf("%d", &data);
+			insertAtBeg(&head, data); // function calling
+			break;
+		case 2:
+			printf("Enter the element you want to insert\n");
+			scanf("%d", &data);
+			insertAtEnd(&head, data); // function calling
+			break;
+		case 3:
+			printf("Enter element after which you want to insert the element\n");
+			scanf("%d", &data);
+			insertAfterSpecific(&head, data);
+			break;
+		case 4:
+			printf("Enter the position where you want to insert the element");
+			scanf("%d", &data);
+			insertAtSpecificPosition(&head, data);
+			break;
+		case 5:
+			traversing(head); // calling
+			break;
+		case 6:
+			printf("Enter the element you want to delete\n");
+			scanf("%d", &data);
+			deleteSpecific(&head, data);
+			break;
+		case 7:
+			printf("Enter the position of element which you want to delete\n");
+			scanf("%d", &data);
+			deleteSpecificPosition(&head, data);
+			break;
 
-        default:
-            printf("Enter the valid choice\n");
-            break;
-        }
-    }
-    return (1);
+		default:
+			printf("Enter the valid choice\n");
+			break;
+		}
+	}
+
 }
 // function decleration of insert at begning
 void insertAtBeg(node **head, int data)
 {
-    temp = (node *)(malloc(sizeof(node)));
-    temp->info = data;
-    temp->next = *head;
-    *head = temp;
-    printf("node insert successfully\n");
+	temp = (node *)(malloc(sizeof(node)));
+	temp->info = data;
+	temp->next = *head;
+	*head = temp;
+	printf("node insert successfully\n");
 }
 // function decleration of traversing
 void traversing(node *head)
 {
-    while (head != NULL)
-    {
-        printf("%d ", head->info);
-        head = head->next;
-    }
+	while (head != NULL)
+	{
+		printf("%d ", head->info);
+		head = head->next;
+	}
 }
 // function decleration of insert at end
 void insertAtEnd(node **head, int data)
 {
-    temp = (node *)(malloc(sizeof(node)));
-    temp->info = data;
-    temp->next = NULL;
-    if (*head == NULL)
-    {
-        *head = temp;
-        printf("node insert successfully\n");
-        return;
-    }
-    node *ptr = *head;
-    while (ptr->next != NULL)
-    {
-        ptr = ptr->next;
-    }
-    ptr->next = temp;
-    printf("node insert successfully\n");
+	temp = (node *)(malloc(sizeof(node)));
+	temp->info = data;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		printf("node insert successfully\n");
+		return;
+	}
+	ptr = *head;
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = temp;
+	printf("node insert successfully\n");
 }
 // function decleration for insert after specific element
 void insertAfterSpecific(node **head, int data)
 {
-    if (*head == NULL)
-    {
-        printf("No element found\n");
-    }
-    node *ptr = *head;
-    while (ptr != NULL)
-    {
-        if (ptr->info == data)
-            break;
-        ptr = ptr->next;
+	if (*head == NULL)
+	{
+		printf("No element found\n");
+	}
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		if (ptr->info == data)
+			break;
+		ptr = ptr->next;
     }
     if (ptr == NULL)
     {
@@ -180,7 +181,7 @@ void deleteSpecific(node **head, int data)
     {
         printf("no element found\n");
     }
-    node *ptr = *head;
+	ptr = *head;
     if (ptr->info == data)
     {
         ptr = *head;
