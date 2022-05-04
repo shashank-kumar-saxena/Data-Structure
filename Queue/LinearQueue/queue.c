69dbd6628ba7fdf752f8e49d736ca101071a98e9
Shashank <shashanksaxena226@gmail.com>
2022-05-04 21:48:21 +0530

    queue

---
+++ Queue/LinearQueue/queue.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct queueType
+{
+    int a[10];
+    int f,r;//here f=front and r=rear
+}queue;
+
+void main()
+{
+    queue q;
+    while (1)
+    {
+        /* code */
+    }
+    
+}
