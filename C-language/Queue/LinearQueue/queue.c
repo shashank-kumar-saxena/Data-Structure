0de38424113fad5e63cdc74b334d3b103c394cd8
Shashank <shashanksaxena226@gmail.com>
2022-05-15 23:26:33 +0530

    Linear Queue final

--- C-language/Queue/LinearQueue/queue.c
+++ C-language/Queue/LinearQueue/queue.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 typedef struct typeQueue
 {
     int a[10];
@@ -9,17 +9,20 @@ typedef struct typeQueue
 void main()
 {
     void enqueue(queue *, int);
+    void dequeue(queue *);
+    void traverse(queue);
     queue q;
     int n = 0, data;
     q.front = q.rear = -1;
     while (1)
     {
-        printf("Enter your choice\n");
+        printf("\nEnter your choice\n");
         printf("1 for insert an element\n");
         printf("2 for delete an element\n");
-        printf("3 for exit\n");
+        printf("3 for travere in list\n");
+        printf("4 for exit\n");
         scanf("%d", &n);
-        if (n == 3)
+        if (n == 4)
         {
             exit(1);
         }
@@ -30,7 +33,12 @@ void main()
             scanf("%d", &data);
             enqueue(&q, data);
             break;
-
+        case 2:
+            dequeue(&q);
+            break;
+        case 3:
+            traverse(q);
+            break;
         default:
             break;
         }
@@ -47,13 +55,13 @@ void enqueue(queue *q, int data)
     {
         q->front = q->rear = 0;
     }
-    else if (q->front > 0 && q->rear == 0)
+    else if (q->front > 0 && q->rear == 9)
     {
         for (int i = q->front; i <= q->rear; i++)
         {
             q->a[i - q->front] = q->a[i];
         }
-        q->rear = q->front - q->rear;
+        q->rear = q->rear - q->front;
         q->front = 0;
         (q->rear)++;
     }
@@ -62,4 +70,29 @@ void enqueue(queue *q, int data)
         (q->rear)++;
     }
     q->a[q->rear] = data;
+    printf("Element Inserted");
+}
+void dequeue(queue *q)
+{
+    if (q->front == q->rear)
+    {
+        q->front = q->rear = -1;
+        printf("Underflow");
+        return;
+    }
+    (q->front)++;
+    printf("Element deleted");
+}
+void traverse(queue q)
+{
+    if (q.front == q.rear)
+    {
+        printf("No Element Found\n");
+        return;
+    }
+    int i = 0;
+    for (i = q.front; i <= q.rear; i++)
+    {
+        printf("%d ", q.a[i]);
+    }
 }
