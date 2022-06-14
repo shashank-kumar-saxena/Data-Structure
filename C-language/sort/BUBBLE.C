978369f115404a8789e80acf6d3e5c1b85b78268
Shashank <shashanksaxena226@gmail.com>
2022-06-14 06:26:15 +0530

    Bubble

--- C-language/sort/BUBBLE.C
+++ C-language/sort/BUBBLE.C
@@ -1,18 +1,29 @@
 #include <stdio.h>
-void insert(int a[], int size);
-int n, i;
-void main()
+int main()
 {
-    printf("Enter the size of array\n");
-    scanf("%d", &n);
-    int arr[n];
-    insert(arr, n);
+    int a[10] = {1, 41, 6, 3, 7, 7, 75, 3, 63, 12};
+    void bubble(int[], int);
+    bubble(a, 10);
 }
-void insert(int a[], int size)
+void bubble(int a[], int size)
 {
-    printf("Enter the element in array one by one\n");
-    for (i = 0; i < size; i++)
+    int i, j, flag = 1;
+    for (i = 1; i < size && flag; i++)
     {
-        scanf("%d", a[i]);
+        flag = 0;
+        for (j = 0; j < (10 - i); j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                flag = 1;
+            }
+        }
+    }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d,", a[i]);
     }
 }
